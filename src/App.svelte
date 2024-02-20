c60457c0332d309dbdde6e358c44dce44f5a3ec5
Aayush Nair <aayushnair321@gmail.com>
2024-02-21 00:10:38 +0530

    PollForm Creation

--- src/App.svelte
+++ src/App.svelte
@@ -23,7 +23,6 @@
 <style>
 	main{
 		max-width: 960px;
-		margin: 40px auto;
-		
+		margin: 40px auto;	
 	}
 </style>
--- src/Shared/Tabs.svelte
+++ src/Shared/Tabs.svelte
@@ -10,8 +10,9 @@
 <div class="tabs">
     <ul>
         {#each items as item}
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
             <li on:click={() => dispatch('tabChange' , item)}>
-                <div class:active = {item === activeItem}>{item}</div>jdimsim
+                <div class:active = {item === activeItem}>{item}</div>
             </li>
         {/each}
     </ul>
